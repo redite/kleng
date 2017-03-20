10b177fba29bd5f33347abc2e3f3cdb90ba4829b
widnyana <widnyana.p@gmail.com>
2017-03-20 19:36:37 +0700

    populate trigged by flag

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/redite/kleng/core"
 	"github.com/redite/kleng/server"
 	"github.com/redite/kleng/utils"
 )
 
+var (
+	mode = flag.String("M", "", "run with `mode`. available mode: 'populate', 'http'")
+)
+
 func main() {
+
+	flag.Parse()
+
 	conf, err := utils.LoadConfig()
 	if err != nil {
 		fmt.Printf("Got error: %s", err.Error())
 		os.Exit(-1)
 	}
 
-	server.RunServer(conf)
+	switch *mode {
+	case "http":
+		log.Fatal(server.RunServer(conf))
+		break
+	case "populate":
+		err = core.Populate()
+		if err != nil {
+			log.Printf("Error Populating: %s\n", err.Error())
+		}
+		break
+	}
+
 }
