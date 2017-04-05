3fe0b4a286b4ff4b50b9b3dd50ed1f57f0b5f2e2
Wayan Jimmy <jimmyeatcrab@gmail.com>
2017-04-06 00:47:44 +0800

    ✨ Initialize uikit

--- resources/assets/js/components/Nav.jsx
+++ resources/assets/js/components/Nav.jsx
@@ -2,40 +2,20 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-          </button>
-          <a className="navbar-brand" href="/">Start Bootstrap</a>
-        </div>
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul className="nav navbar-nav">
-            <li>
-              <a href="/">About</a>
-            </li>
-            <li>
-              <a href="/">Services</a>
-            </li>
-            <li>
-              <a href="/">Contact</a>
-            </li>
-          </ul>
-        </div>
+    <div className="uk-navbar-container" data-uk-sticky="media: 960">
+      <div className="uk-container uk-container-expand">
+        <nav className="uk-navbar">
+          <div className="uk-navbar-left">
+            <a href="#" className="uk-navbar-item uk-logo">Kleng</a>
+          </div>
+          <div className="uk-navbar-right">
+            <ul className="uk-navbar-nav uk-visible@m">
+              <li><a href="#">About</a></li>
+            </ul>
+          </div>
+        </nav>
       </div>
-    </nav>
+    </div>
   );
 };
 
--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+UIkit.use(Icons);
 
 window.$ = window.jQuery = require('jquery');
 
