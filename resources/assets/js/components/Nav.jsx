14f6a4571cbbdfba641f3809edfceca7ae1792eb
Wayan Jimmy <jimmyeatcrab@gmail.com>
2017-04-09 12:13:04 +0800

    ✨ Add sidebar and content

--- resources/assets/js/components/Nav.jsx
+++ resources/assets/js/components/Nav.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div className="uk-navbar-container" data-uk-sticky="media: 960">
+    <div className="uk-navbar-container tm-navbar-container" data-uk-sticky="media: 960" data-uk-navbar>
       <div className="uk-container uk-container-expand">
         <nav className="uk-navbar">
-          <div className="uk-navbar-left">
+          <div className="uk-navbar-left uk-light">
             <a href="#" className="uk-navbar-item uk-logo">Kleng</a>
-          </div>
-          <div className="uk-navbar-right">
-            <ul className="uk-navbar-nav uk-visible@m">
-              <li><a href="#">About</a></li>
-            </ul>
           </div>
         </nav>
       </div>
